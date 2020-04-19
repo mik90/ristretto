952b000326f2933ef25a249030e1368fa89163f0
Mike Kaliman <kaliman.mike@gmail.com>
2020-04-19 16:01:50 -0400

    Connect to tcp socket w/ boost asio

--- src/DecoderInterface.cpp
+++ src/DecoderInterface.cpp
@@ -1,17 +1,76 @@
+#include <boost/asio.hpp>
+#include <spdlog/spdlog.h> 
+#include <spdlog/sinks/basic_file_sink.h>
+#include <fmt/core.h>
+
+#include <iostream>
+
 #include "DecoderInterface.hpp"
 
 namespace mik {
-    void DecoderInterface::connect() {
-        using boost::asio::ip::tcp;
-        boost::asio::io_context ioContext;
-        tcp::socket sock(ioContext);
-        tcp::resolver res(ioContext);
-        boost::asio::connect(sock, resolver.resolve(host, port));
+    DecoderInterface::DecoderInterface() : ioContext_(), socket_(ioContext_) {
+        try {
+            logger_ = spdlog::basic_logger_mt("DecodeInterfaceLogger", "logs/client.log");
+        }
+        catch(const spdlog::spdlog_ex& e) {
+            std::cerr << "Log init failed with spdlog_ex: " << e.what() << std::endl;
+        }
         
-        // Write out data to socket
-        //boost::asio::write(sock, boost::asio::buffer(outBuf, outLen));
-
-        // Read in reply 
-        //boost::asio::read(sock, boost::asio::buffer(inBuf, inLen));
+        logger_->info("DecoderInterface created.");
+        logger_->set_level(spdlog::level::level_enum::debug);
+        logger_->debug("Debug-level logging enabled");
     }
+
+    void DecoderInterface::connect(std::string_view host, std::string_view port) {
+
+        boost::asio::ip::tcp::resolver res(ioContext_);
+
+        boost::asio::ip::tcp::resolver::results_type endpoints;
+        try {
+            endpoints = res.resolve(host, port);
+
+        } catch(const boost::system::system_error& e) {
+            logger_->error("Error {}", e.what());
+            return;
+        }
+
+        logger_->info("Retrieved endpoints");
+
+        try {
+            boost::asio::connect(socket_, endpoints);
+            logger_->info("Connected to {}:{}", host, port);
+
+        } catch (const boost::system::system_error& e) {
+            logger_->info("Caught exception on connect(): {}", e.what());
+            return;
+        }
+        
+    }
+
+    size_t DecoderInterface::sendAudio(const std::unique_ptr<char>& buffer, size_t nBytes) {
+        // Write out data to socket
+        const auto buf = boost::asio::const_buffer(buffer.get(), nBytes);
+        const auto bytesWritten = boost::asio::write(socket_, buf);
+        logger_->debug("Wrote out {} bytes of audio", bytesWritten);
+        return bytesWritten;
+    }
+
+    std::string DecoderInterface::readResults() {
+        // Read in reply 
+        boost::asio::streambuf streamBuf;
+        const auto bytesRead = boost::asio::read_until(socket_, streamBuf, "\n");
+        logger_->debug("Read in {} bytes of results");
+        const auto bufIter = streamBuf.data();
+        const std::string result(boost::asio::buffers_begin(bufIter),
+                                 boost::asio::buffers_begin(bufIter) + static_cast<long>(bytesRead));
+        return result;
+    }
+}
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+    mik::DecoderInterface iface;
+    iface.connect("127.0.0.1", "5050");
+
+
+    return 0;
 }
--- src/DecoderInterface.hpp
+++ src/DecoderInterface.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <boost/asio.hpp>
+#include <spdlog/spdlog.h> 
 
-constexpr long MaxLength = 1024;
+constexpr long MaxLength = 65535;
 
 namespace mik {
     // Used for sending audio to the Kaldi online2-tcp-nnet3-decode-faster server
 
     // Given a stream of audio, send it over TCP
+
     class DecoderInterface {
         public:
-            void connect();
+            DecoderInterface();
+            void connect(std::string_view host, std::string_view port);
+            size_t sendAudio(const std::unique_ptr<char>& buffer, size_t nBytes);
+            std::string readResults();
         private:
-            boost::asio::ip::tcp socket_;
-    }
+            std::shared_ptr<spdlog::logger> logger_;
+            boost::asio::io_context         ioContext_;
+            boost::asio::ip::tcp::socket    socket_;
+    };
 
 }

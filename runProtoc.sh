bad02dd5a4756346aaca587d7823b4a331ff982c
mik90 <kaliman.mike@gmail.com>
2020-05-28 20:07:29 -0400

    Trying out alpine

--- runProtoc.sh
+++ runProtoc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # I can't get the cmake custom command to work so this is just a workaround
 cd build
+mkdir -p src
 protoc --grpc_out ./src --cpp_out ./src -I ../src/protos \
  --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin ../src/protos/ristretto.proto

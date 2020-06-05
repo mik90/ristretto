e0f9401626f5424a50f04690494147efdb5a4423
Mike Kaliman <kaliman.mike@gmail.com>
2020-06-04 22:37:45 -0400

    Build should work now

--- buildClient.sh
+++ buildClient.sh
@@ -12,21 +12,4 @@ run_build()
     cmake --build . --parallel $(nproc)
 }
 
-run_tests()
-{
-    echo "Running buildClient.sh::run_tests()"
-    echo "PWD:$PWD"
-    # Could use ctest binary, but the executable itself is more verbose on CLI
-    #ctest . -E "USER_INPUT|REQUIRES_SERVER"
-
-    cd ./bin
-    TEST_BINARY="./ClientTest"
-    GTEST_ARGS=" --gtest_filter=-*USER_INPUT*:*REQUIRES_SERVER* "
-    test -f $TEST_BINARY || { echo "$TEST_BINARY does not exist, have you built it yet?"; exit 1; }
-    CMD="$TEST_BINARY $GTEST_ARGS"
-    eval $CMD
-}
-
-# Main
 run_build
-run_tests
